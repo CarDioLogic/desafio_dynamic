6eed2f88a746048c65d39d7705b9d49c9c261de4
João Silva <61765922+CarDioLogic@users.noreply.github.com>
2026-04-25 16:50:05 +0100

    https change

--- frontend/core/constants.ts
+++ frontend/core/constants.ts
@@ -1,2 +1,2 @@
 export const baseUrlDev = "http://127.0.0.1:8000/api";
-export const baseUrl = "http://developers-challenge.great-site.net/api";
+export const baseUrl = "https://developers-challenge.great-site.net/api";
